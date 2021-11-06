cdb3b55f13f2623cafb5bd2982658a5b5face21e
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2021-11-06 12:34:49 +0100

    dropdown

--- src/components/molecules/NavigationLink/NavigationLink.js
+++ src/components/molecules/NavigationLink/NavigationLink.js
@@ -1,8 +1,8 @@
 import { Link, Icon } from './NavigationLink.styles';
 
-const NavigationLink = ({ text, link, icon, horizontal }) => {
+const NavigationLink = ({ text, link, icon, horizontal, onClick, id }) => {
     return ( 
-        <Link a href={link} horizontal={horizontal}>
+        <Link a href={link} horizontal={horizontal} onClick={onClick} id={id} >
             {text ? <span text={text}>{text}</span> : null}
             {icon ? <Icon src={icon} alt="arrow-down"/> : null}
         </Link>
--- src/components/organisms/Filters/Filters.js
+++ src/components/organisms/Filters/Filters.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from 'react';
 import arrowDown from 'assets/images/arrow-down.png';
 import NavigationLink from 'components/molecules/NavigationLink/NavigationLink';
 import { Filter, StyledList } from './Filtets.styles';
-
-
+import styled from 'styled-components';
 
 const Filters = () => {
     const [width, setWidth] = useState(window.innerWidth);
@@ -22,16 +21,79 @@ const Filters = () => {
 
     let isMobile = width >= 768 ? false : true;
 
+    const StyledDropdown = styled.ul`
+        margin-left: 1rem;
+        margin-top: 2rem;
+    `;
+
+    const StyledListItem = styled.li`
+        list-style: none;
+        cursor: pointer;
+
+        &:nth-child(n+2) {
+            margin-top: 1rem;
+        }
+    `;
+
+const navItems = [
+    {
+        id: 1,
+        text: 'Category',
+        icon: arrowDown,
+    },
+    {
+        id: 2,
+        text: 'Collection',
+        icon: arrowDown,
+    },
+    {
+        id: 3,
+        text: 'Price',
+        icon: arrowDown,
+    },
+    {
+        id: 4,
+        text: 'Color',
+        icon: arrowDown,
+    }
+];
+
+    const [isOpen, setIsOpen] = useState({
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+    });
+
+    const handleIsOpen = (id) => {
+        setIsOpen({
+            ...isOpen,
+            [id]: !isOpen[id],
+        })
+    }
+
+    const DropdownMenu = ({id}) => {
+        return (    
+            <StyledDropdown>
+                <StyledListItem>Boots</StyledListItem>
+                <StyledListItem>Jackets and blazers</StyledListItem>
+                <StyledListItem>Jeans and chinos</StyledListItem>
+                <StyledListItem>Accessories</StyledListItem>
+            </StyledDropdown>
+        )
+    }
 
     return (
         <Filter> 
             {isMobile ? 
             <NavigationLink text={'Filter'} icon={arrowDown} /> : null }
             {!isMobile ? <StyledList>
-                <NavigationLink text={'Category'} icon={arrowDown} />
-                <NavigationLink text={'Collection'} icon={arrowDown} />
-                <NavigationLink text={'Price'} icon={arrowDown} />
-                <NavigationLink text={'Color'} icon={arrowDown} />
+            {navItems.map(({ text, icon, id }) => (
+                <>
+                    <NavigationLink text={text} icon={icon} id={id} onClick={() => handleIsOpen(id)} />
+                    {isOpen[id] ? <DropdownMenu id={id} /> : null}
+                </>
+            ))}
             </StyledList> : null}
         </Filter>
      );
