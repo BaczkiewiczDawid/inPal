423df6e21db4ecac4252a9a795c70692e58d746e
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2021-11-22 21:21:26 +0100

    selecting item

--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,18 @@
 import Home from 'components/templates/Home/Home'
+import Item from 'components/templates/Item/Item';
 import GlobalStyle from 'assets/styles/GlobalStyles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/item/:id" element={<Item />} />
+        </Routes>
+      </Router>
     </>
 
   )
